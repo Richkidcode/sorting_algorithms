f6aeb3d069c97e383f42b0cc57f68a29a6cdd3b5
Felix Kamau Kinyuru <144423962+Richkidcode@users.noreply.github.com>
2024-02-24 13:15:57 +0300

    102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,91 +1,36 @@
-
-#include "sort.h"
-
-/**
- * list_len - function returns length of list
- * @list: head of list
- *
- * Return: length
- */
-size_t list_len(listint_t *list)
+void counting_sort(int *array, size_t size)
 {
-        size_t len = 0;
+        int *count, *sorted, max, i;
 
-        while (list)
-        {
-                len++;
-                list = list->next;
-        }
-        return (len);
-}
-
-/**
- * switch_nodes - function swaps nodes at pointer p with the following node
- * @list: head of list
- * @p: pointer to node
- */
-void switch_nodes(listint_t **list, listint_t **p)
-{
-        listint_t *one, *two, *three, *four;
-
-        one = (*p)->prev;
-        two = *p;
-        three = (*p)->next;
-        four = (*p)->next->next;
-        two->next = four;
-        if (four)
-                four->prev = two;
-        three->next = two;
-        three->prev = two->prev;
-	if (one)
-                one->next = three;
-        else
-                *list = three;
-        two->prev = three;
-        *p = three;
-}
-
-/**
- *  cocktail_sort_list - function sorts a doubly linked list using
- * the cocktail sort algorithm
- * @list: pointer to list
- */
-void cocktail_sort_list(listint_t **list)
-{
-        listint_t *p;
-        int sorted = 0;
-
-        if (!list || !*list || list_len(*list) < 2)
+        if (array == NULL || size < 2)
                 return;
-        p = *list;
-        while (!sorted)
+
+        sorted = malloc(sizeof(int) * size);
+        if (sorted == NULL)
+                return;
+        max = get_max(array, size);
+        count = malloc(sizeof(int) * (max + 1));
+        if (count == NULL)
         {
-                sorted = 1;
-                while (p->next)
-                {
-                        if (p->n > p->next->n)
-                        {
-                                sorted = 0;
-                                switch_nodes(list, &p);
-                                print_list(*list);
-                        }
-                        else
-                                p = p->next;
-                }
-                if (sorted)
-                        break;
-                p = p->prev;
-                while (p->prev)
-                {
-                        if (p->n < p->prev->n)
-                        {
-                                sorted = 0;
-                                p = p->prev;
-                                switch_nodes(list, &p);
-                                print_list(*list);
-                        }
-                        else
-                                p = p->prev;
-                }
+                free(sorted);
+                return;
         }
-}
+
+        for (i = 0; i < (max + 1); i++)
+                count[i] = 0;
+        for (i = 0; i < (int)size; i++)
+                count[array[i]] += 1;
+        for (i = 0; i < (max + 1); i++)
+                count[i] += count[i - 1];
+        print_array(count, max + 1);
+
+        for (i = 0; i < (int)size; i++)
+        {
+                sorted[count[array[i]] - 1] = array[i];
+                count[array[i]] -= 1;
+        }
+
+        for (i = 0; i < (int)size; i++)
+                array[i] = sorted[i];
+
+        free(sorted);
